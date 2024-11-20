955486e769be800355fedb17694c67603b9deae3
Valouz <valent1.arn@ik.me>
2024-11-20 15:20:05 +0100

    Update pages.jsx

--- components/pages.jsx
+++ components/pages.jsx
@@ -4,6 +4,7 @@ import {MasterSearchInput} from "@/components/inputs";
 import {Section} from "@/components/wrappers";
 import {useSearchParams} from "next/navigation";
 import useSWR from "swr";
+import {TrainingCard} from "@/components/cards";
 
 const HomePage = () => {
     return (
@@ -22,10 +23,26 @@ const SearchPage = () => {
     const fetcher = (...args) => fetch(...args).then((res) => res.json());
     const {data: trainings} = useSWR("https://la-lab4ce.univ-lemans.fr/masters-stats/api/rest/formations", fetcher);
 
-    const trainingsFiltered = trainings ? trainings.filter(training => training.ville === query) : null;
+    const trainingsFiltered = trainings ? trainings.filter(training => training.ville === query.toUpperCase() && training.parcours !== null) : null;
 
     return (
-        <>{trainingsFiltered ? JSON.stringify(trainingsFiltered) : "Chargement..."}</>
+        <>
+            <Section className={"pb-0"}>
+                <MasterSearchInput/>
+            </Section>
+            <Section className={"flex flex-row gap-2 flex-wrap"}>
+                {
+                    trainingsFiltered ? (
+                        trainingsFiltered.length > 0 ? (
+                            trainingsFiltered.map((training) => (
+                                <TrainingCard training={training}/>
+                            ))
+                        ) : "Aucun résultat"
+
+                    ) : "Chargement..."
+                }
+            </Section>
+        </>
     );
 }
 
